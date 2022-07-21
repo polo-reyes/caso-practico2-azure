44f94c64437db9f1b4da54111d909d10605d89ed
polo-reyes <38890322+polo-reyes@users.noreply.github.com>
2022-07-20 17:28:59 -0700

    terraform multiple vms creation

--- terraform/correccion-vars.tf
+++ terraform/correccion-vars.tf
@@ -1,23 +1,36 @@
 variable "location" {
   type = string
   description = "Región de Azure donde crearemos la infraestructura"
-  default = "<YOUR REGION>" 
+  default = "uksouth" 
 }
 
 variable "storage_account" {
   type = string
   description = "Nombre para la storage account"
-  default = "<STORAGE ACCOUNT NAME>"
+  default = "debain_storage_account"
 }
 
 variable "public_key_path" {
   type = string
   description = "Ruta para la clave pública de acceso a las instancias"
-  default = "~/.ssh/id_rsa.pub" # o la ruta correspondiente
+  default="C:/Users/polor/Documents/Documentos/UNIR/azure/id_rsa.pub"
+  #default = "~/.ssh/id_rsa.pub" # o la ruta correspondiente
 }
 
 variable "ssh_user" {
   type = string
   description = "Usuario para hacer ssh"
-  default = "<SSH USER>"
+  default = "azureuser"
+}
+
+variable "vm_size" {
+  type=string
+  description="Tamaño de la máquina virtual"
+  default="Standard_D1_v2" # GB, CPUs
+}
+
+variable "vms" {
+  description = "VMs a crear"
+  type=list(string)
+  default = [ "master", "worker1", "NFS" ]
 }
--- terraform/recursos.tf
+++ terraform/recursos.tf
@@ -10,38 +10,85 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "mySubnet" {
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "vnic"
+resource "azurerm_network_interface" "myNic" {
+  count = length(var.vms)
+  name                = "vnic-${var.vms[count.index]}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
+    name                          = "ipconfig-${var.vms[count.index]}"
+    subnet_id                     = azurerm_subnet.mySubnet.id
+    private_ip_address_allocation = "Dynamic" #"Static"
+    #private_ip_address = "10.0.1.${count.index + 10}"
+    public_ip_address_id = azurerm_public_ip.myPublicIp1[count.index].id
   }
 }
 
+
+
+resource "azurerm_public_ip" "myPublicIp1" {
+  count = length(var.vms)
+  name="vmip1-${var.vms[count.index]}"
+  location=azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method = "Dynamic"
+  sku="Basic"
+}
+
+resource "azurerm_network_security_group" "mySecGroup" {
+  count = length(var.vms)
+  name="sshtraffic-${var.vms[count.index]}"
+  location=azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule = [{
+    access                                     = "Allow"
+    description                                = "SSH Rule"
+    destination_address_prefix                 = "*"
+    destination_address_prefixes               = []
+    destination_port_range                     = "22"
+    destination_port_ranges                    = []
+    direction                                  = "Inbound"
+    name                                       = "SSH Rule"
+    priority                                   = 1001
+    protocol                                   = "Tcp"
+    source_address_prefix                      = "*"
+    source_address_prefixes                    = []
+    source_port_range                          = "*"
+    source_port_ranges                         = []
+    source_application_security_group_ids      = []
+    destination_application_security_group_ids = []
+  }]
+}
+
+resource "azurerm_network_interface_security_group_association" "mySecGroupAssociation1" {
+  count = length(var.vms)
+  network_interface_id = azurerm_network_interface.myNic[count.index].id
+  network_security_group_id = azurerm_network_security_group.mySecGroup[count.index].id
+}
+
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm1"
+  count = length(var.vms)
+  name                = "vm2-${var.vms[count.index]}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_F2"
+  size                = var.vm_size #"Standard_F2"
   admin_username      = "azureuser"
   network_interface_ids = [
-    azurerm_network_interface.nic.id,
+    azurerm_network_interface.myNic[count.index].id,
   ]
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("C:/Users/polor/Documents/Documentos/UNIR/azure/id_rsa.pub")
   }
 
   os_disk {
